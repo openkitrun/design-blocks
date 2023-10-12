e6a59ea836fe5615c2a1729ac6adf8b7064f28e7
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2023-10-11 18:34:18 -0600

    rfc(@blocks-*): export modules

--- packages/@blocks-unstyled/scripts/preparePublish.js
+++ packages/@blocks-unstyled/scripts/preparePublish.js
@@ -57,5 +57,7 @@ fs.removeSync(path.join(__dirname, "../src"));
 fs.removeSync(path.join(__dirname, "../CHANGELOG.md"));
 fs.removeSync(path.join(__dirname, "../package.json"));
 fs.removeSync(path.join(__dirname, "../README.md"));
-fs.moveSync(path.join(rootDirOutputPath), path.join(__dirname, ".."));
+fs.copySync(rootDirOutputPath, path.join(__dirname, ".."));
+fs.removeSync(rootDirOutputPath);
+//fs.moveSync(path.join(rootDirOutputPath), path.join(__dirname, ".."));
 fs.removeSync(path.join(__dirname, "../build"));
