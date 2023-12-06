0ba87105f8f05acfcac2883e53525ab19307381a
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2023-12-05 23:15:26 -0600

    fix: fixed instance Stack

--- packages/@blocks-primitives/src/Stack/Stack.tsx
+++ packages/@blocks-primitives/src/Stack/Stack.tsx
@@ -1,58 +1,14 @@
 import * as React from 'react';
 
-import block from '@design-blocks/block';
+import { __DEV__ } from '@design-blocks/utils';
 
-import { StyleFunctionMode, styleFunction } from '@design-blocks/system';
-import { __DEV__, camelCase } from '@design-blocks/utils';
+import type { StackProps } from './Stack.types';
 
-import { Box } from '../Box';
-import { variants } from './Stack.utils';
-
-import type { IStackStyleValue, StackProps } from './Stack.types';
-
-const StackBlock = block(Box, {
-  shouldForwardProp: (prop) => prop !== 'theme' && prop !== 'sx' && prop !== 'as',
-})<Omit<StackProps, 'direction'>>(
-  ({
-    flexDirection: flexDirectionProps = 'column',
-    alignItems: alignItemsProps = 'stretch',
-    justifyContent: justifyContentProps = 'flexStart',
-    flexWrap: flexWrapProps = 'nowrap',
-    theme,
-    sx,
-    ...styleProps
-  }): IStackStyleValue => {
-    const flexDirection = variants.flexDirection[
-      camelCase(flexDirectionProps) as keyof typeof variants.flexDirection
-    ] as IStackStyleValue['flexDirection'];
-
-    const alignItems = variants.alignItems[
-      camelCase(alignItemsProps) as keyof typeof variants.alignItems
-    ] as IStackStyleValue['alignItems'];
-
-    const flexWrap = variants.flexWrap[
-      camelCase(flexWrapProps) as keyof typeof variants.flexWrap
-    ] as IStackStyleValue['flexWrap'];
-
-    const justifyContent = variants.justifyContent[
-      camelCase(justifyContentProps) as keyof typeof variants.justifyContent
-    ] as IStackStyleValue['justifyContent'];
-
-    return {
-      display: 'flex',
-      flexDirection,
-      alignItems,
-      justifyContent,
-      flexWrap,
-      ...styleFunction('Box', theme, { ...styleProps }, StyleFunctionMode.PROPS),
-      ...styleFunction('Box', theme, sx, StyleFunctionMode.SX),
-    };
-  },
-);
-function Stack({ direction = 'column', ...props }: StackProps) {
-  return <StackBlock flexDirection={direction} {...props} />;
+import { createStack } from './createStack';
+function Stack({ ...props }: StackProps) {
+  const StackBlock = createStack();
+  return <StackBlock {...props} />;
 }
-
 if (__DEV__) {
   Stack.displayName = 'Block.Stack';
 }
--- packages/@blocks-system/src/styleFunction/index.ts
+++ packages/@blocks-system/src/styleFunction/index.ts
@@ -1,3 +1,5 @@
 export * from './getValuesTokens';
 export * from './styleFunction';
 export * from './validateProperties';
+export * from './styleFunctionProps';
+export * from './styleFunctionSx';
